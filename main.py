d5d4ddfb90a120b4c7e36470dd360f2e0bc2ff1c
Rémi Barbaroux <remibarbaroux@MacBook-Pro-de-Remi-2.local>
2025-11-18 22:32:23 +0100

    oups

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from src.nn_interface import MLPModel
 from src.preprocessing import preprocessing, data_spam, data_diabetes
 from sklearn.metrics import confusion_matrix, classification_report
 
-def main() :
+def nn() :
     samples = preprocessing(data=data_diabetes, test_size=0.15, validation_size=0.15)
     model = MLPModel()
     model.train(samples)
     model.benchmark(samples.X_test, samples.y_test)
 
 if __name__ == "__main__":
-    main()
+    pass
 
