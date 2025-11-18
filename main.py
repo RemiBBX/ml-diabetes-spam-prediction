6f933fe283a76a41c9b5449107e660bb005d9587
Baptiste Bayle <b.bayle.dev@gmail.com>
2025-11-18 19:30:52 +0100

    Add benchmark function

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
-from sklearn.metrics import balanced_accuracy_score, classification_report
-
-from preprocessing import data_diabetes, preprocessing
+from preprocessing import data_spam, preprocessing
 from src.knn import KNNModel
 
 
 def main():
-    samples = preprocessing(data=data_diabetes, test_size=0.3, validation_size=0.1)
-    model = KNNModel(k=3)
+    samples = preprocessing(data=data_spam, test_size=0.3, validation_size=0.1)
+    model = KNNModel()
     model.train(x=samples.X_train, y=samples.y_train)
-    report = classification_report(
-        y_pred=model.predict(samples.X_test),
-        y_true=samples.y_test,
-    )
-    acc = balanced_accuracy_score(y_pred=model.predict(samples.X_test), y_true=samples.y_test, adjusted=True)
-    print(acc)
-    print(report)
+    model.benchmark(x=samples.X_validation, y=samples.y_validation)
 
 
 if __name__ == "__main__":
