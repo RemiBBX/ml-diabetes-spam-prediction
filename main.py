8d2cbbcedf1eed0486927d83f998d7e7537c6f0d
Baptiste Bayle <b.bayle.dev@gmail.com>
2025-11-20 19:35:23 +0100

    Fix preprocessing and move notebooks and add notebook dependency

--- main.py
+++ main.py
@@ -19,11 +19,11 @@ def knn():
 
 
 def random_forest():
-    samples = preprocessing(data=data_diabetes, test_size=0.3, validation_size=0.1)
+    samples = preprocessing(data=data_spam, test_size=0.3, validation_size=0.1)
     model = RForest()
     model.train(x=samples.X_train, y=samples.y_train)
     model.benchmark(x=samples.X_validation, y=samples.y_validation)
 
 
 if __name__ == "__main__":
-    nn()
+    knn()
--- src/model_interface.py
+++ src/model_interface.py
@@ -4,12 +4,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
 from sklearn.metrics import (
-    accuracy_score,
     classification_report,
     confusion_matrix,
     f1_score,
-    precision_score,
-    recall_score,
 )
 
 
@@ -25,41 +22,25 @@ class LearningModelInterface(ABC):
     def benchmark(self, x: np.ndarray, y: np.ndarray):
         y_pred = self.predict(x)
 
-        acc = accuracy_score(y, y_pred)
-        prec = precision_score(y, y_pred)
-        rec = recall_score(y, y_pred)
         f1 = f1_score(y, y_pred)
 
-        print("\n===== BENCHMARK DU MODÈLE =====")
-        print(f"Accuracy  : {acc:.4f}")
-        print(f"Precision : {prec:.4f}")
-        print(f"Recall    : {rec:.4f}")
-        print(f"F1-score  : {f1:.4f}")
-
-        print("\n===== Classification Report =====")
         report_dict = classification_report(y, y_pred, output_dict=True)
-        print(classification_report(y, y_pred))
 
         classes = list(report_dict.keys())[:-3]  # drop 'accuracy' 'macro avg' 'weighted avg'
         metrics = ["precision", "recall", "f1-score"]
 
         data = np.array([[report_dict[c][metric] for metric in metrics] for c in classes])
 
-        plt.figure(figsize=(7, 3))
-        sns.heatmap(data, annot=True, cmap="Greens", xticklabels=metrics, yticklabels=classes, fmt=".3f")
-        plt.title("Classification Report (visualisation)")
-        plt.show()
+        fig, axis = plt.subplots(1, 3, figsize=(15, 4))
+        sns.heatmap(data, annot=True, cmap="Greens", xticklabels=metrics, yticklabels=classes, fmt=".3f", ax=axis[0])
+        axis[0].set_title("Classification Report (visualisation)")
 
         # CONFUSION MATRIX
         cm = confusion_matrix(y, y_pred)
-        plt.figure(figsize=(5, 4))
-        sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
-        plt.title("Matrice de confusion")
-        plt.xlabel("Prédiction")
-        plt.ylabel("Vérité")
-        plt.show()
-
-        print("\n===== IMPORTANCE DES FEATURES =====")
+        sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", ax=axis[1])
+        axis[1].set_title("Matrice de confusion")
+        axis[1].set_xlabel("Prédiction")
+        axis[1].set_ylabel("Vérité")
 
         n_features = x.shape[1]
         importances = []
@@ -80,17 +61,14 @@ class LearningModelInterface(ABC):
             importance = baseline_score - perm_score
             importances.append(importance)
 
-            print(f"Feature {i} : importance = {importance:.4f}")
-
         importances = np.array(importances)
         importances = np.maximum(importances, 0)  # clamp < 0 à 0
         importances /= importances.sum() + 1e-9  # normalisation
 
         # IMPORTANCES
-        plt.figure(figsize=(7, 4))
-        sns.barplot(x=np.arange(len(importances)), y=importances)
-        plt.title("Importance des features (Permutation)")
-        plt.xlabel("Features")
-        plt.ylabel("Importance normalisée")
-        plt.grid(axis="y")
+        sns.barplot(x=np.arange(len(importances)), y=importances, ax=axis[2])
+        axis[2].set_title("Importance des features (Permutation)")
+        axis[2].set_xlabel("Features")
+        axis[2].set_ylabel("Importance normalisée")
+        axis[2].grid(axis="y")
         plt.show()
